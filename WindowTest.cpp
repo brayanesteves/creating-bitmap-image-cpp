e84e1d61bc0f446e701e0d8c1b441d8b288c75ae
Brayan Esteves <brayan.esteves93@gmail.com>
2024-05-07 14:15:19 -0400

    Window[Creating] - Creating Window [07/05/2024]

---
+++ WindowTest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "./h/Window.h"
+
+int main() {
+    std::cout << "Creating Window\n";
+
+    Window* pWindow = new Window();
+
+    bool running = true;
+    while(running) {
+        if(!pWindow->ProcessMessages()) {
+            std::cout << "Closing Window\n";
+            running = false;
+        }
+        // Render.
+        Sleep(10);
+    }
+    delete pWindow;
+    return 0;
+}
